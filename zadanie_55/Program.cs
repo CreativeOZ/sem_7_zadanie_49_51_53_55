93b9c7e8f7fdd2afc11d8370ec0c19fe9b8b7f2d
CreativeOZ <zagriaoleg@gmail.com>
2022-03-19 17:17:03 +0300

    55: Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.

---
+++ zadanie_55/Program.cs
@@ -0,0 +1,29 @@
+﻿// 55: Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+Console.Clear();
+
+Console.Write("Колличество срок: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Колличество стобцов: ");
+int n = int.Parse(Console.ReadLine());
+int [,] array = new int [m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int z = 0; z < n; z++)
+    {
+        array[i,z] = new Random().Next(0,10);
+        Console.Write(array[i,z] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int z = 0; z < n; z++)
+{
+    double sum = 0;
+    for (int i = 0; i < m; i++)
+    {
+        sum = sum + array[i,z];
+    }
+    Console.WriteLine($"Cреднее арифметическое каждого из столбцов: {sum / m}" + " ");
+}
